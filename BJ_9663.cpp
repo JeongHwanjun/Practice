86d5bda709a3383f401250ddaa9c310411ecc86a
JeongHwanjun <ghkswnsdlwjd@naver.com>
2021-07-19 21:22:21 +0900

    2021_0719

--- BJ_9663.cpp
+++ BJ_9663.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int pos[15]={0};
 int cnt=0;
+int n;
 
-int abs(int a)
+void repeat(int col,int row)
 {
-    return (a>0 ? a:-a);
-}
-
-void repeat(int n,int queen)
-{
-    if(queen<=0){
-        cnt++;
-        return;
+    bool flag=true;
+    for(int i=0;i<row;i++){
+        if(pos[i]==col || abs(pos[i]-col)==abs(i-row)) flag=false;
     }
 
-    for(int i=1;i<=n;i++){
-        bool flag=false;
-        for(int j=1;j<=n;j++){
-            if(pos[j]!=0 && (pos[j]-i==0 || abs(pos[j]-i)==abs(j-i))) flag=true;
+    if(flag){
+        if(row>=n-1){
+            cnt++;
+            return;
         }
-        if(!flag){
-            cout<<i<<endl;
-            pos[n-queen+1]=i;
-            repeat(n,queen-1);
+        pos[row]=col;
+        for(int i=0;i<n;i++){
+            repeat(i,row+1);
         }
+        pos[row]=0;
     }
+
+    return;
 }
 
 int main()
 {
-    int n;
-
     cin>>n;
 
-    repeat(n,n);
+    for(int i=0;i<n;i++) repeat(i,0);
 
     cout<<cnt;
 
